2db63e0b17788ac76044ca63e419a6f6334f1c50
Matsuyanagi <ef58ef64@gmail.com>
2017-06-29 00:35:47 +0900

    typo prime test → prime number test assert_equal → assert

--- test_mr_prime.rb
+++ test_mr_prime.rb
@@ -11,21 +11,20 @@ class TC_MrPrime < Test::Unit::TestCase
 		end
 	end
 	
-	# 64bit prime test
+	# 64bit prime number test
 	def test_64bit_prime
-		assert_equal( true, 0xffffffffffffffad.mr_prime? )
+		assert( 0xffffffffffffffad.mr_prime? )
 	end
 	
-	# 65bit prime test
+	# 65bit prime number test
 	def test_65bit_prime
 		assert_equal( true, 0x1ffffffffffffffcf.mr_prime? )
 	end
 	
-	# 10^36 prime test
+	# 10^36 prime number test
 	def test_10_36_prime
 		assert_equal( true, 999999999999999999999999999999999841.mr_prime? )
 	end
-	
 	
 end
 
